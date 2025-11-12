456e91d9e0a8e69a7d2bdb81f7afdf23a597771a
puck-arthur <arthur_scheffer@hotmail.com>
2025-11-12 21:53:30 +0000

    Adicionando o formatador Prettier

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório criado durante os estudos no curso.dev onde pretendo recriar o Tabnews para aprender mais sobre programação.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Oi, benzinho! Se você me ama, dá uma risadinha.</h1>
+  return <h1>Oi, benzinho! Se você me ama, dá uma risadinha.</h1>;
+}
+
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
